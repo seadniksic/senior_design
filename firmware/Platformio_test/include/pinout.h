ecdfe117bccd7a1810cb5809eb10431df7105b5d
ericsherman4 <esherman2018@gmail.com>
2023-02-12 21:05:31 -0500

    added config file and seperated test vs production pinout defs

--- firmware/Platformio_test/include/pinout.h
+++ firmware/Platformio_test/include/pinout.h
@@ -1,35 +1,39 @@
 #ifndef PINOUT_H_
 #define PINOUT_H_
 
+#include "config.h"
+
+
+#if USE_PRODUCTION_PINS
+
 //Notes regarding PWM https://www.pjrc.com/teensy/td_pulse.html
 // Looks pins 2 and 3 are on the same time timer
 // and so are 28 and 29
 // all of them have a default frequency of 4.482 kHz
 
+
+/* MOTOR DRIVER PINS */
 // GOAL IS HAVE ALL OF THEM BE ON THEIR OWN TIMERS IN CASE I NEED THAT
 
 // BR = Back Right
-#define BR_IN1_PIN 30 // MOVING TO PIN 20
-#define BR_IN2_PIN 31 // MOVING TO PIN 21
-#define BR_EN_PIN 2 //MOVING TO PIN 5 (updated on fritzing)
+#define BR_IN1_PIN 20 // MOVING TO PIN 20
+#define BR_IN2_PIN 21 // MOVING TO PIN 21
+#define BR_EN_PIN 5 //MOVING TO PIN 5 (updated on fritzing)
 
 // BL = Back Left
 #define BL_IN3_PIN 26
 #define BL_IN4_PIN 27
-#define BL_EN_PIN 3 // MOVING TO PIN 23 (updated on fritzing)
+#define BL_EN_PIN 23 // MOVING TO PIN 23 (updated on fritzing)
 
 // FL = Front Left
-#define FL_IN1_PIN 36 //MOVING TO PIN 41 (updated on fritzing)
-#define FL_IN2_PIN 37 //MOVING TO PIN 32 (updated on fritzing)
+#define FL_IN1_PIN 41 //MOVING TO PIN 41 (updated on fritzing)
+#define FL_IN2_PIN 32 //MOVING TO PIN 32 (updated on fritzing)
 #define FL_EN_PIN 28
 
 // FR = Front Right
 #define FR_IN3_PIN 34
-#define FR_IN4_PIN 35 //MOVING TO PIN 40 (updated on fritizing)
-#define FR_EN_PIN 29 //MOVING TO PIN 9 (updated on fritzing)
-
-
-
+#define FR_IN4_PIN 40 //MOVING TO PIN 40 (updated on fritizing)
+#define FR_EN_PIN 9 //MOVING TO PIN 9 (updated on fritzing)
 
 /* i2c buses */
 #define I2C_SCL_GYRO_PIN 19
@@ -48,7 +52,6 @@
 // WARNING! Pins 0, 5 and 37 share the same internal crossbar connections and 
 // are as such exclusive...pick one or the other. Same thing applies to pins
 //  1 / 36 and 5 / 37.
-
 
 #define BR_ENC_PHASEA_PIN 2
 #define BR_ENC_PHASEB_PIN 3
@@ -79,46 +82,75 @@
 
 
 /* pins for battery monitoring / control */
-#warning "complete these pins battery control"
+#pragma message("complete these pins battery control")
 
 
 /* pins for interacting with jetson? */
-#warning "complete these pins for jetson"
+#pragma message("complete these pins for jetson")
+
+#endif
 
 
-/* Joystick pinout
-overlap with these pin numbers dont matter right 
-now because these won't be used on the final model
 
-LEFT CONNECTOR
-_________________________
-| left  | up   |  LED    |
-|_______|______|_________|  
-| right | down |  FNC SW |
-|_______|______|_________|
+#if USE_TEST_ROVER_PINS
 
-RIGHT CONNECTOR
-_________________________
-| LX    | LY   |  VCC    |
-|_______|______|_________|  
-| RX    | RY   |  GND    |
-|_______|______|_________|
+    /* Joystick pinout
+    overlap with these pin numbers dont matter right 
+    now because these won't be used on the final model
 
-The RX and LY mappings correspond to the physical pin name, not the direction
-that the joysticks will actually be moving IRL (since the joystick PCB is rotated)
-The code will need to account for this and flip them
-*/ 
+    LEFT CONNECTOR
+    _________________________
+    | left  | up   |  LED    |
+    |_______|______|_________|  
+    | right | down |  FNC SW |
+    |_______|______|_________|
 
-#define DPAD_UP_PIN 25
-#define DPAD_DOWN_PIN 11
-#define DPAD_RIGHT_PIN 12
-#define DPAD_LEFT_PIN 24
-#define JOY_LX_PIN 
-#define JOY_LY_PIN 
-#define JOY_RX_PIN 
-#define JOY_RY_PIN
-#define JOY_LED_PIN 33
-#define JOY_HOMEBTN_PIN 
+    RIGHT CONNECTOR
+    _________________________
+    | LX    | LY   |  VCC    |
+    |_______|______|_________|  
+    | RX    | RY   |  GND    |
+    |_______|______|_________|
 
+    The RX and LY mappings correspond to the physical pin name, not the direction
+    that the joysticks will actually be moving IRL (since the joystick PCB is rotated)
+    The code will need to account for this and flip them
+    */ 
+
+    #define DPAD_UP_PIN 25
+    #define DPAD_DOWN_PIN 11
+    #define DPAD_RIGHT_PIN 12
+    #define DPAD_LEFT_PIN 24
+    #define JOY_LX_PIN 
+    #define JOY_LY_PIN 
+    #define JOY_RX_PIN 
+    #define JOY_RY_PIN 
+    #define JOY_LED_PIN 33
+    #define JOY_HOMEBTN_PIN 
+
+    #error "fix pin numbers"
+
+    /* Motor Driver pinout  */
+    // BR = Back Right
+    #define BR_IN1_PIN 30 
+    #define BR_IN2_PIN 31 
+    #define BR_EN_PIN 2 
+
+    // BL = Back Left
+    #define BL_IN3_PIN 26
+    #define BL_IN4_PIN 27
+    #define BL_EN_PIN 3 
+
+    // FL = Front Left
+    #define FL_IN1_PIN 36 
+    #define FL_IN2_PIN 37 
+    #define FL_EN_PIN 28
+
+    // FR = Front Right
+    #define FR_IN3_PIN 34
+    #define FR_IN4_PIN 35 
+    #define FR_EN_PIN 29 
+
+#endif
 
 #endif 
