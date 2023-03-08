583cdd678e35e45ccfa1747eb42aa806eadbf3dd
jsb19227 <jsb19227@gmail.com>
2023-03-07 19:04:30 -0500

    fine i guess you are my lil pogchamp

--- network_stuff/rover/src/testImageTransmit.cpp
+++ network_stuff/rover/src/testImageTransmit.cpp
@@ -9,16 +9,20 @@ int main()
     TransmitData<unsigned char> testTransmit(CLIENT_IP, WIFI_IMAGE_PORT);
 
     cv::Mat img = cv::imread("/home/joeyblack/Documents/School/ECE1896/senior_design/network_stuff/rover/src/testSources/testImage.jpg", cv::IMREAD_COLOR);
-    if(img.empty())
-    {
-        std::cout << "Could not read the image: " << std::endl;
-        return 1;
-    }
-    std::cout << "Image Size: " << img.total() * img.elemSize() << std::endl;
+    cv::Mat img2 = cv::imread("/home/joeyblack/Documents/School/ECE1896/senior_design/network_stuff/rover/src/testSources/testImage2.jpg", cv::IMREAD_COLOR);
+    cv::Mat img3 = cv::imread("/home/joeyblack/Documents/School/ECE1896/senior_design/network_stuff/rover/src/testSources/testImage_3.jpg", cv::IMREAD_COLOR);
+
 
     while(1)
     {
         testTransmit.sendPayload(img.data, img.total() * img.elemSize());
         std::cout << "Sent image" << std::endl;
+        sleep(2);
+        testTransmit.sendPayload(img2.data, img2.total() * img2.elemSize());
+        std::cout << "Sent image" << std::endl;
+        sleep(2);
+        testTransmit.sendPayload(img3.data, img3.total() * img3.elemSize());
+        std::cout << "Sent image" << std::endl;
+        sleep(2);
     }
 }
