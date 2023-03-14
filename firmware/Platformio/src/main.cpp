62c5d531c564a826c83618881b4e6fbc62a679e3
ericsherman4 <esherman2018@gmail.com>
2023-03-14 19:14:51 -0400

    fixed some print issues and added code for driving

--- firmware/Platformio/src/main.cpp
+++ firmware/Platformio/src/main.cpp
@@ -26,7 +26,7 @@ void main_prog()
 
   // setup
   // joystick_init();
-  // locomotion_init();
+  locomotion_init();
   // bno055::init();
   HWSERIAL.begin(115200);
 
@@ -127,18 +127,24 @@ void main_prog()
         // returns enum class Buttons derived from uint32_t type.
         // so should be able to bit shift.
         // theres 14 fields in buttons
-        for(uint8_t i = 0; i < 14; i++)
+        // going to 16 to just print all 2 bytes
+        for(uint8_t i = 0; i < 16; i++)
         {
+          if(i % 4 == 0)
+          {
+            Serial.print(" ");
+          }
           if((btn_status >> i) & 0x1)
           {
-            Serial.printf("%u: 1, ", i);
+            // Serial.printf("%u: 1, ", i);
+            Serial.print("1");
           }
           else
           {
-            Serial.printf("%u: 0, ", i);
+            // Serial.printf("%u: 0, ", i);
+            Serial.print("0");
           }
         }
-        // Serial.println();
 
         int16_t LJOY_X = (int16_t)js_in.get_LJOY_X();
         int16_t LJOY_Y = (int16_t)js_in.get_LJOY_Y();
@@ -147,7 +153,10 @@ void main_prog()
         uint8_t TR = (uint8_t)js_in.get_TR();
         uint8_t TL = (uint8_t)js_in.get_TL();
 
-        Serial.printf("LJOY_X: %6.0d, LJOY_Y: %6.0d, RJOY_X: %6.0d, RJOY_Y: %6.0d, TR: %u, TL: %u", LJOY_X, LJOY_Y, RJOY_X, RJOY_Y, TR, TL);
+        // Serial.println();
+        Serial.printf(", LJOY_X: %6.0d, LJOY_Y: %6.0d, ", LJOY_X, LJOY_Y);
+        Serial.printf("RJOY_X: %6.0d, RJOY_Y: %6.0d, ", RJOY_X, RJOY_Y);
+        Serial.printf("TR: %3u, TL: %3u", TR, TL);
         Serial.println();
 
       }
@@ -194,8 +203,33 @@ void main_prog()
     }
     #endif
 
+    uint32_t btn_status = (uint32_t)js_in.get_button();
+
+    if((btn_status >> 6) & 0x1)
+    {
+      drive_forward();
+    }
+    else if((btn_status >> 7) & 0x1)
+    {
+      drive_backward();
+    }
+    else if((btn_status >> 8) & 0x1)
+    {
+      drive_left();
+    }
+    else if(btn_status >> 9 & 0x1)
+    {
+      drive_right();
+    }
+    else{
+      all_axis_off();
+    }
+
     read_buffer.clear();
     write_buffer.clear();
+    
+
+
 
     delay(10);
 
