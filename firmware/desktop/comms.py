3e1039a73c3da37a2b4d7e62576a25561ba1c0df
ericsherman4 <esherman2018@gmail.com>
2023-03-14 19:17:12 -0400

    adding parsing and sending for the rest of the joystick inputs

--- firmware/desktop/comms.py
+++ firmware/desktop/comms.py
@@ -3,14 +3,13 @@ from time import sleep
 import serial
 import uart_messages_pb2
 import evdev
-# import atexit
 import sys
 from evdev import categorize, ecodes
 from timeit import default_timer as timer
-import struct
+import random
 
 class g:
-    update_rate = 1 # in hz
+    update_rate = 20 # in hz
     sleep_time = 1/ update_rate
     joy_name = "Logitech Gamepad F710"
     test_input = False
@@ -33,19 +32,17 @@ class Joystick:
             308 : 8,
             315 : 16,
             314 : 32,
-            # omitting DPAD
+
+            "UP" : 64,
+            "DOWN" : 128,
+            "LEFT" : 256,
+            "RIGHT" : 512,
             318 : 1024,
             317 : 2048,
             311 : 4096,
             310 : 8192
         }
 
-        self.DPAD_LUT = {
-            "UP" : 64,
-            "DOWN" : 128,
-            "LEFT" : 256,
-            "RIGHT" : 512
-        }
 
     def print_dev_capabilities(self, verbose_ouput : bool):
         if self.dev != None:
@@ -130,6 +127,12 @@ if __name__ == "__main__":
     # data = struct.pack("H", 0)
 
     btn_data = 0
+    ljoy_x_data = 0
+    ljoy_y_data = 0
+    rjoy_x_data = 0
+    rjoy_y_data = 0
+    tr_data = 0
+    tl_data = 0
 
     start_time = timer()
     end_time = None
@@ -151,9 +154,44 @@ if __name__ == "__main__":
                 # dpad left right is 16
                 if event.code == 16:
                     # dpad right is pressed
-                    pass
-                    # if event.value == 1:
-                    #     btn_data = btn_data | joystick.DPAD_LUT["RIGHT"] 
+                    if event.value == 1:
+                        btn_data = btn_data | joystick.CODE_LUT["RIGHT"]
+                        btn_data = btn_data & ~joystick.CODE_LUT["LEFT"]
+                    elif event.value == -1:
+                        btn_data = btn_data & ~joystick.CODE_LUT["RIGHT"]
+                        btn_data = btn_data | joystick.CODE_LUT["LEFT"]
+                    else:
+                        btn_data = btn_data & ~joystick.CODE_LUT["RIGHT"]
+                        btn_data = btn_data & ~joystick.CODE_LUT["LEFT"]
+                elif event.code == 17:
+                    # dpad right is pressed
+                    if event.value == 1:
+                        btn_data = btn_data | joystick.CODE_LUT["DOWN"]
+                        btn_data = btn_data & ~joystick.CODE_LUT["UP"]
+                    elif event.value == -1:
+                        btn_data = btn_data & ~joystick.CODE_LUT["DOWN"]
+                        btn_data = btn_data | joystick.CODE_LUT["UP"]
+                    else:
+                        btn_data = btn_data & ~joystick.CODE_LUT["DOWN"]
+                        btn_data = btn_data & ~joystick.CODE_LUT["UP"]
+                elif event.code == 4:
+                    # rjoy y
+                    rjoy_y_data = event.value
+                elif event.code == 3:
+                    #rjoy x
+                    rjoy_x_data = event.value
+                elif event.code == 1:
+                    #ljoy y
+                    ljoy_y_data = event.value
+                elif event.code == 0:
+                    #ljoy x
+                    ljoy_x_data = event.value
+                elif event.code == 5:
+                    # RT
+                    tr_data = event.value
+                elif event.code == 2:
+                    # LT
+                    tl_data = event.value
                 
         
         #check if its time to send data
@@ -174,26 +212,28 @@ if __name__ == "__main__":
             b.extend(g.sync_byte.to_bytes(length=1, byteorder='little', signed=False))
 
             #prepare the proto message to send
-            print("button data", btn_data)
+            # print("button data", btn_data)
             proto_msg.button = btn_data
+            proto_msg.LJOY_X = ljoy_x_data
+            proto_msg.LJOY_Y = ljoy_y_data            
+            proto_msg.RJOY_X = rjoy_x_data
+            proto_msg.RJOY_Y = ljoy_y_data
+            proto_msg.TR = tr_data
+            proto_msg.TL = tl_data
+
+
             # serialize the data
             proto_msg_serialized = proto_msg.SerializeToString()
 
             # store the length of the message and add it to be sent
             msg_len = len(proto_msg_serialized)
-            # fix it sending nothing when data is equal to zero
-            if(msg_len == 0):
-                msg_len = 2
-                b.extend(msg_len.to_bytes(length=1, byteorder='little', signed = False))
-                zero = 0
-                b.extend(zero.to_bytes(length=2, byteorder = 'little', signed= False))
-            else: 
-                # print("proto msg", proto_msg_serialized)
-                # print(msg_len)
-                b.extend(msg_len.to_bytes(length=1, byteorder='little', signed = False))
-                
-                # add the proto data
-                b.extend(proto_msg_serialized)
+            
+            # print("proto msg", proto_msg_serialized)
+            print("msg length",msg_len)
+            b.extend(msg_len.to_bytes(length=1, byteorder='little', signed = False))
+            
+            # add the proto data
+            b.extend(proto_msg_serialized)
             print(b)
             sp.write(b)
     
