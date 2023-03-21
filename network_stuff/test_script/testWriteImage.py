e2fcbd6be53ef0dc4db0138655901d577e413b22
jsb19227 <45880580+jsb19227@users.noreply.github.com>
2023-03-21 19:17:47 -0400

    whyyy

--- network_stuff/test_script/testWriteImage.py
+++ network_stuff/test_script/testWriteImage.py
@@ -5,7 +5,6 @@ if __name__=='__main__':
     data = cv2.imread('testImage_3.jpg')
     data = data.tobytes()
     size = int(len(data)).to_bytes(8, byteorder='little', signed=False)
-    print(size.hex())
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = "127.0.0.1"
