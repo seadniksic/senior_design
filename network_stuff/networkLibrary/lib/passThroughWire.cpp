beae318c76162c6641ed24727d67b2df41839388
jsb19227 <45880580+jsb19227@users.noreply.github.com>
2023-03-23 18:29:22 -0400

    comments

--- network_stuff/networkLibrary/lib/passThroughWire.cpp
+++ network_stuff/networkLibrary/lib/passThroughWire.cpp
@@ -2,6 +2,17 @@
 #define PASSTHROUGHWIRE_CPP
 
 #include "passThroughWire.h"
+/*
+This code defines the implementation of a PassThroughWire class. The class constructor takes in several arguments, including receive and transmit ports, transmit IP, buffer size, and name.
+
+The constructor initializes the class's member variables, including creating instances of ReceiveData and TransmitData classes, which are used for receiving and transmitting data respectively. It also creates a unique pointer to a char array with the size of bufferSize.
+
+The update() function is the main method of the PassThroughWire class. It continuously receives data from the server using the getData() method of the ReceiveData class until it receives a non-zero return value. It then sends the received payload to the client using the sendPayload() method of the TransmitData class.
+
+The code is also wrapped in an include guard, which prevents the header file from being included more than once in a translation unit.
+
+Overall, this code defines a class that can be used to pass data through a network connection using receive and transmit ports, as well as a buffer for storing data.
+*/
 
 PassThroughWire::PassThroughWire(const int receivePort, const int transmitPort, const char *transmitIP, const int bufferSize, std::string name)
 {
