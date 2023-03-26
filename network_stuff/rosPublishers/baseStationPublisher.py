4e3a798d3b4abd81c777a2076e838c337f995497
jsb19227 <jsb19227@gmail.com>
2023-03-26 16:36:09 -0400

    sadge

--- network_stuff/rosPublishers/baseStationPublisher.py
+++ network_stuff/rosPublishers/baseStationPublisher.py
@@ -10,13 +10,18 @@ if __name__ == '__main__':
     try:
         rclpy.init()
 
+        executor = rclpy.executors.MultiThreadedExecutor()
         nodeList = []
         nodeList.append(ImagePublisher())
+        executor.add_node(nodeList[0])
         nodeList.append(PointCloudPublisher())
+        executor.add_node(nodeList[1])
         nodeList.append(CameraPositionPublisher())
+        executor.add_node(nodeList[2])
         nodeList.append(RoverStatusPublisher())
+        executor.add_node(nodeList[3])
 
-        rclpy.spin(nodeList)
+        rclpy.spin(executor)
         
         for node in nodeList:
             node.shutdownNode()
