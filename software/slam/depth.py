12f1611c4a469cc876599ce76b9926207985a458
Sead Niksic <seaddniksic@gmail.com>
2023-03-29 12:13:26 -0400

    ????

--- software/slam/depth.py
+++ software/slam/depth.py
@@ -3,20 +3,34 @@ import rospy
 from sensor_msgs.msg import Image
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "127.0.0.1"
-port = 8084
-sock.connect((host, port))
+from cv_bridge import CvBridge
+import cv2
+import time
+import numpy as np
 
 
-def image_callback(msg):
-    sendData = msg.tobytes()
-    size = int(len(sendData)).to_bytes(8, byteorder="little", signed=False)
-    sock.sendall(size)
-    sock.sendall(sendData)
 
+class ImageTransmitter:
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect(("127.0.0.1", 8084))
+        self.oldImage = np.zeros((480, 640, 3), np.uint8)
+    def image_callback(self, msg):
+        sendData = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        # start =  time.time()
+        __, sendData = cv2.imencode('.jpg', sendData, [int(cv2.IMWRITE_JPEG_QUALITY), 10])
+        # print(time.time() - start)
+        sendData = sendData.tobytes()
+        size = int(len(sendData)).to_bytes(8, byteorder="little", signed=False)
+        # print("SENT", len(sendData))
+        self.sock.sendall(size)
+        self.sock.sendall(sendData)    
 
-rospy.init_node("image_subscriber")
-rospy.Subscriber("/camera/image", Image, image_callback)
-rospy.spin()
-sock.close()
+if __name__ == '__main__':
+    imageSender = ImageTransmitter()
+    rospy.init_node("image_subscriber")
+    rospy.Subscriber("/camera/color/image_raw", Image, imageSender.image_callback)
+    rospy.spin()
+    imageSender.sock.close()
+
