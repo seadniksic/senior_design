e1c605bbc693f8a845c8e5392d079843b5910a2b
jsb19227 <45880580+jsb19227@users.noreply.github.com>
2023-03-29 11:02:14 -0400

    point cloud pubby

--- network_stuff/rosPublishers/pointCloudPublisher.py
+++ network_stuff/rosPublishers/pointCloudPublisher.py
@@ -1,9 +1,43 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2, PointField
-import sensor_msgs.point_cloud2 as pc2
 import numpy as np
 import cv2, socket
+import struct
+
+def merge_rgb_fields(cloud_arr):
+    '''Takes an array with named np.uint8 fields 'r', 'g', and 'b', and returns an array in
+    which they have been merged into a single np.float32 'rgb' field. The first byte of this
+    field is the 'r' uint8, the second is the 'g', uint8, and the third is the 'b' uint8.
+    This is the way that pcl likes to handle RGB colors for some reason.
+    '''
+    r = np.asarray(cloud_arr[:, :, 0], dtype=np.uint32)
+    g = np.asarray(cloud_arr[:, :, 1], dtype=np.uint32)
+    b = np.asarray(cloud_arr[:, :, 2], dtype=np.uint32)    
+    rgb_arr = np.array((r << 16) | (g << 8) | (b << 0), dtype=np.uint32)
+
+    # # not sure if there is a better way to do this. i'm changing the type of the array
+    # # from uint32 to float32, but i don't want any conversion to take place -jdb
+    rgb_arr.dtype = np.float32
+
+    # create a new array, without r, g, and b, but with rgb float32 field
+    # new_dtype = []
+    # for field_name in ['r', 'g', 'b']:
+    #     field_type, field_offset = cloud_arr.dtype.fields[field_name]
+    #     field_type = 
+    #     if field_name not in ('r', 'g', 'b'):
+    #         new_dtype.append((field_name, field_type))
+    # new_dtype.append(('rgb', np.float32))
+    # new_cloud_arr = np.zeros(cloud_arr.shape, new_dtype)    
+
+    # # fill in the new array
+    # for field_name in new_cloud_arr.dtype.names:
+    #     if field_name == 'rgb':
+    #         new_cloud_arr[field_name] = rgb_arr
+    #     else:
+    #         new_cloud_arr[field_name] = cloud_arr[field_name]
+        
+    return rgb_arr
 
 class MinimalPublisher(Node):
 
@@ -28,29 +62,47 @@ class MinimalPublisher(Node):
         size = int.from_bytes(size, 'little')
         if size <= 0:
             return
-        msg = self.client.recv(1400)
-        while len(msg) < size:
-            msg += self.client.recv(min(1400, size - len(msg)))
+        xyzSize = self.client.recv(8)
+        xyzSize = int.from_bytes(xyzSize, 'little')
+
+        msg = self.client.recv(xyzSize)
+        while len(msg) < xyzSize:
+            msg += self.client.recv(xyzSize - len(msg))
         
+        rgbSize = self.client.recv(8)
+        rgbSize = int.from_bytes(rgbSize, 'little')
+        
+        while(len(msg) < xyzSize + rgbSize):
+            msg += self.client.recv(xyzSize + rgbSize - len(msg))
 
         buffer = np.frombuffer(msg, np.uint8)
-        image = cv2.imdecode(buffer, cv2.IMREAD_COLOR)
+        xyzArray = cv2.imdecode(buffer[:xyzSize], cv2.IMREAD_COLOR)
+        rgbArray = cv2.imdecode(buffer[xyzSize:], cv2.IMREAD_COLOR)
+        xyzArray = xyzArray.astype(np.float32)
+        rgbSingleField = merge_rgb_fields(rgbArray)
+        # pointCloudArray = np.append(xyzArray, rgbSingleField)
+        # print(xyzArray.dtype)
+        # print(rgbSingleField.dtype)
+
+        combinedArray = rgbSingleField.tobytes()
+        print(len(combinedArray))
+
         pointCloudMsg = PointCloud2()
-        pointCloudMsg.header.stamp = rclpy.Time.now()
-        pointCloudMsg.header.frame_id = "my_frame_id"
-        pointCloudMsg.height = 1
-        pointCloudMsg.width = image.shape[0]
-        pointCloudMsg.fields = [PointField('x', 0, PointField.FLOAT32, 1),
-              PointField('y', 4, PointField.FLOAT32, 1),
-              PointField('z', 8, PointField.FLOAT32, 1),
-              PointField('r', 12, PointField.FLOAT32, 1),
-              PointField('g', 16, PointField.FLOAT32, 1),
-              PointField('b', 20, PointField.FLOAT32, 1)]
+        pointCloudMsg.header.stamp = self.get_clock().now().to_msg()
+        pointCloudMsg.header.frame_id = "map"
+        pointCloudMsg.height = 480
+        pointCloudMsg.width = 640
+        fields = [PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
+              PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
+              PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
+              PointField(name='rgb', offset=12, datatype=PointField.FLOAT32, count=1)
+        ]
+        pointCloudMsg.fields = fields
         pointCloudMsg.is_bigendian = False
-        pointCloudMsg.point_step = 24
-        pointCloudMsg.row_step = pointCloudMsg.point_step * image.shape[0]
-        pointCloudMsg.is_dense = True
-        pointCloudMsg.data = image.astype(np.float32).tobytes()
+        pointCloudMsg.point_step = 16
+        pointCloudMsg.row_step = 20480
+        pointCloudMsg.is_dense = False
+        pointCloudMsg.data = xyzArray.tobytes() + rgbSingleField.tobytes()
         self.publisher_.publish(pointCloudMsg)
 
 
