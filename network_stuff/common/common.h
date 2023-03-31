f19aadf4b1004e1ae3014ea389bae18bc0f5f8f7
jsb19227 <45880580+jsb19227@users.noreply.github.com>
2023-03-30 20:51:12 -0400

    pubby daddy

--- network_stuff/common/common.h
+++ network_stuff/common/common.h
@@ -40,9 +40,9 @@
 #define MAX_PACKET_SIZE 1400
 
 #define IMAGE_DEBUG 0
-#define POINT_CLOUD_DEBUG 1
+#define POINT_CLOUD_DEBUG 0
 #define STATUS_DEBUG 0
-#define COMMANDS_DEBUG 0
+#define COMMANDS_DEBUG 1
 #define POISITON_DEBUG 0 
 
 #endif
