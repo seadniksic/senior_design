8fe343770ed924ba96e8c13c0cda7136c0ed1097
jsb19227 <jsb19227@gmail.com>
2023-04-01 16:03:51 -0400

    map compression

--- network_stuff/rosPublishers/pointCloudPublisher.py
+++ network_stuff/rosPublishers/pointCloudPublisher.py
@@ -1,51 +1,15 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import PointCloud2, PointField, Image
-import numpy as np
-import cv2, socket
-import struct
-from cv_bridge import CvBridge
+import lz4.frame
+from rtabmap_ros.msg import MapData
+import socket
 
-def merge_rgb_fields(cloud_arr):
-    '''Takes an array with named np.uint8 fields 'r', 'g', and 'b', and returns an array in
-    which they have been merged into a single np.float32 'rgb' field. The first byte of this
-    field is the 'r' uint8, the second is the 'g', uint8, and the third is the 'b' uint8.
-    This is the way that pcl likes to handle RGB colors for some reason.
-    '''
-    r = np.asarray(cloud_arr[:, :, 0], dtype=np.uint32)
-    g = np.asarray(cloud_arr[:, :, 1], dtype=np.uint32)
-    b = np.asarray(cloud_arr[:, :, 2], dtype=np.uint32)    
-    rgb_arr = np.array((r << 16) | (g << 8) | (b << 0), dtype=np.uint32)
-
-    # # not sure if there is a better way to do this. i'm changing the type of the array
-    # # from uint32 to float32, but i don't want any conversion to take place -jdb
-    rgb_arr.dtype = np.float32
-
-    # create a new array, without r, g, and b, but with rgb float32 field
-    # new_dtype = []
-    # for field_name in ['r', 'g', 'b']:
-    #     field_type, field_offset = cloud_arr.dtype.fields[field_name]
-    #     field_type = 
-    #     if field_name not in ('r', 'g', 'b'):
-    #         new_dtype.append((field_name, field_type))
-    # new_dtype.append(('rgb', np.float32))
-    # new_cloud_arr = np.zeros(cloud_arr.shape, new_dtype)    
-
-    # # fill in the new array
-    # for field_name in new_cloud_arr.dtype.names:
-    #     if field_name == 'rgb':
-    #         new_cloud_arr[field_name] = rgb_arr
-    #     else:
-    #         new_cloud_arr[field_name] = cloud_arr[field_name]
-        
-    return rgb_arr
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(PointCloud2, 'pointCloudStream', 10)
-        self.imagePub = self.create_publisher(Image, 'pointCloudImageStream', 10)
+        self.publisher_ = self.create_publisher(MapData, 'mapDataStream', 10)
         timer_period = 0.016666  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
@@ -60,57 +24,23 @@ class MinimalPublisher(Node):
 
 
     def timer_callback(self):
+
         size = self.client.recv(8)
         size = int.from_bytes(size, 'little')
         if size <= 0:
             return
-        xyzSize = self.client.recv(8)
-        xyzSize = int.from_bytes(xyzSize, 'little')
 
-        msg = self.client.recv(xyzSize)
-        while len(msg) < xyzSize:
-            msg += self.client.recv(xyzSize - len(msg))
-        
-        rgbSize = self.client.recv(8)
-        rgbSize = int.from_bytes(rgbSize, 'little')
-        
-        while(len(msg) < xyzSize + rgbSize):
-            msg += self.client.recv(xyzSize + rgbSize - len(msg))
+        msg = self.client.recv(size)
+        while len(msg) < size:
+            msg += self.client.recv(size - len(msg))
 
-        buffer = np.frombuffer(msg, np.uint8)
-        xyzArray = cv2.imdecode(buffer[:xyzSize], cv2.IMREAD_COLOR)
-        rgbArray = cv2.imdecode(buffer[xyzSize:], cv2.IMREAD_COLOR)
+        newMapData = lz4.frame.decompress(msg)
+        mapData = MapData()
+        mapData.header = None #Need to implemenmt
+        mapData.id = None #need to implement
+        mapData.data = newMapData
 
-        bridge = CvBridge()
-        rosMsg = bridge.cv2_to_imgmsg(rgbArray, encoding="passthrough")
-        self.imagePub.publish(rosMsg)
-
-        xyzArray = xyzArray.astype(np.float32)
-        rgbSingleField = merge_rgb_fields(rgbArray)
-        # pointCloudArray = np.append(xyzArray, rgbSingleField)
-        # print(xyzArray.dtype)
-        # print(rgbSingleField.dtype)
-
-        combinedArray = rgbSingleField.tobytes()
-        print(len(combinedArray))
-
-        pointCloudMsg = PointCloud2()
-        pointCloudMsg.header.stamp = self.get_clock().now().to_msg()
-        pointCloudMsg.header.frame_id = "map"
-        pointCloudMsg.height = 480
-        pointCloudMsg.width = 640
-        fields = [PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
-              PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
-              PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
-              PointField(name='rgb', offset=12, datatype=PointField.FLOAT32, count=1)
-        ]
-        pointCloudMsg.fields = fields
-        pointCloudMsg.is_bigendian = False
-        pointCloudMsg.point_step = 16
-        pointCloudMsg.row_step = 20480
-        pointCloudMsg.is_dense = False
-        pointCloudMsg.data = xyzArray.tobytes() + rgbSingleField.tobytes()
-        self.publisher_.publish(pointCloudMsg)
+        self.publisher_.publish(mapData)
 
 
 def main(args=None):
--- software/slam/pointCloudData.py
+++ software/slam/pointCloudData.py
@@ -1,58 +1,19 @@
 import rospy
-import sensor_msgs.point_cloud2 as pc2
-import numpy as np
-import cv2
+import lz4.frame
 import socket
-import ros_numpy as rnp
+from rtabmap_ros.msg import MapData
+import socket
+
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "127.0.0.1"
 port = 8085
 sock.connect((host, port))
 
 # Define a callback function to receive the PointCloud2 message
-def callback(point_cloud_msg):
-    # Define the fields to extract from the PointCloud2 message
-    # print(len(point_cloud_msg.data['x']))
-    points_array = rnp.point_cloud2.pointcloud2_to_xyz_array(point_cloud_msg, remove_nans=False)
-    # rgb = ros_numpy.point_cloud2.pointcloud2_to_rgb_array(point_cloud_msg)
-    # # Convert the PointCloud2 message to a generator of tuples
-    # point_cloud_generator = pc2.read_points(point_cloud_msg, field_names=["x","y","z","rgb"], skip_nans=True)
-    # # Convert the generator to a list of tuples
-    # points_list = list(point_cloud_generator)
-    # # Convert the list of tuples to a NumPy array
-    # points_array = np.array(points_list)
-    # print(points_array.shape)
-    #points_array = xyz + rgb
-    #print(len(point_cloud_msg.data))
-    points = rnp.point_cloud2.pointcloud2_to_array(point_cloud_msg)
-    cloud_array = rnp.point_cloud2.split_rgb_field(points)
-    # rgb = rgb["r"] + rgb['g'] + rgb['b']
-    xyz = np.zeros(cloud_array.shape + (3,), dtype=np.uint8)
-    rgb = np.zeros(cloud_array.shape + (3,), dtype=np.uint8)
-    xyz[...,0] = cloud_array['x']
-    xyz[...,1] = cloud_array['y']
-    xyz[...,2] = cloud_array['z']
-
-    rgb[...,0] = cloud_array['r']
-    rgb[...,1] = cloud_array['g']
-    rgb[...,2] = cloud_array['b']
-
-    #print(points_array.shape)
-    #new_array = rnp.point_cloud2.array_to_pointcloud2(points_array)
-    # print(len(new_array.data))
-    # print(points_array.dtype)
-    _, sendData = cv2.imencode('.jpg', xyz, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-    __, sendData2 = cv2.imencode('.jpg', rgb, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-    # sendData = points_array
-    #print(sendData.dtype)
-    sendData = sendData.tobytes()
-    sendData = int(len(sendData)).to_bytes(8, byteorder="little", signed=False) + sendData
-    sendData2 = sendData2.tobytes()
-    sendData2 = int(len(sendData2)).to_bytes(8, byteorder="little", signed=False) + sendData2
-    # print(len(sendData), len(sendData2))
-    sendData = sendData + sendData2
+def callback(msg):
     
-    
+    sendData = lz4.frame.compress(msg.data)
     # print(len(sendData))
     size = int(len(sendData)).to_bytes(8, byteorder="little", signed=False)
     sock.sendall(size)
@@ -63,7 +24,7 @@ def callback(point_cloud_msg):
 rospy.init_node('point_cloud_converter')
 
 # Subscribe to the PointCloud2 topic
-rospy.Subscriber('/camera/depth_registered/points', pc2.PointCloud2, callback)
+rospy.Subscriber('/camera/depth_registered/points', MapData, callback)
 
 # Spin the ROS node to receive messages
 rospy.spin()
