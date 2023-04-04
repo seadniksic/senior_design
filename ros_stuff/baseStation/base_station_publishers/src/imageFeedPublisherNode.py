8ce1bf25c643760d4e2a52c028e2dd983172b11e
jsb19227 <jsb19227@gmail.com>
2023-04-04 18:31:16 -0400

    asdf

--- ros_stuff/baseStation/base_station_publishers/src/imageFeedPublisherNode.py
+++ ros_stuff/baseStation/base_station_publishers/src/imageFeedPublisherNode.py
@@ -1,43 +1,3 @@
-# #!/usr/bin/env python
-
-# import rospy, cv2, socket, select
-# from std_msgs.msg import String
-# from cv_bridge import CvBridge
-# from sensor_msgs.msg import Image
-# import numpy as np
-
-# class ImagePublisher:
-#     def __init__(self):
-#         rospy.init_node('imagePublisher')
-#         self.publisher_ = rospy.Publisher('imageFeedStream', Image, queue_size=10)
-#         self.subscriber_ = rospy.Subscriber('cameraFeedNetworkNodePublisher', String, self.callback)
-
-#         self.bridge = CvBridge()
-#         rospy.spin()
-    
-#     def callback(self, msg):
-#         try: 
-#             data = bytearray(msg.data, 'utf-8')
-#             buffer = np.frombuffer(data, np.uint8)
-#             image = cv2.imdecode(buffer, cv2.IMREAD_COLOR)
-
-#             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#             rosMsg = self.bridge.cv2_to_imgmsg(image, encoding="passthrough")
-#             self.publisher_.publish(rosMsg)
-
-#         except Exception as e:
-#             print("Image Publisher ran into " + str(e))
-
-# if __name__ == '__main__':
-#     try:
-#         imageNode = ImagePublisher()
-
-#     except KeyboardInterrupt:
-#         pass
-
-#     finally:
-#         print("Shutting down node")
-
 #!/usr/bin/env python
 
 import rospy, cv2, socket, select
@@ -46,26 +6,35 @@ from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 import numpy as np
 
-bridge = CvBridge()
+class ImagePublisher:
+    def __init__(self):
+        rospy.init_node('imagePublisher')
+        self.publisher_ = rospy.Publisher('imageFeedStream', Image, queue_size=10)
+        self.subscriber_ = rospy.Subscriber('cameraFeedNetworkNodePublisher', String, self.callback)
 
-rospy.init_node('imagePublisher')
-publisher_ = rospy.Publisher('imageFeedStream', Image, queue_size=10)
+        self.bridge = CvBridge()
+        rospy.spin()
+    
+    def callback(self, msg):
+        try: 
+            data = bytearray(msg.data, 'utf-8')
+            buffer = np.frombuffer(data, np.uint8)
+            image = cv2.imdecode(buffer, cv2.IMREAD_COLOR)
 
-def callback(msg):
-    try: 
-        data = bytearray(msg.data, 'utf-8')
-        buffer = np.frombuffer(data, np.uint8)
-        image = cv2.imdecode(buffer, cv2.IMREAD_COLOR)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            rosMsg = self.bridge.cv2_to_imgmsg(image, encoding="passthrough")
+            self.publisher_.publish(rosMsg)
 
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        rosMsg = self.bridge.cv2_to_imgmsg(image, encoding="passthrough")
-        publisher_.publish(rosMsg)
+        except Exception as e:
+            print("Image Publisher ran into " + str(e))
 
-    except Exception as e:
-        print("Image Publisher ran into " + str(e))
+if __name__ == '__main__':
+    try:
+        imageNode = ImagePublisher()
 
-subscriber_ = rospy.Subscriber('cameraFeedNetworkNodePublisher', String, callback)
-rospy.spin()
+    except KeyboardInterrupt:
+        pass
 
-
+    finally:
+        print("Shutting down node")
 
