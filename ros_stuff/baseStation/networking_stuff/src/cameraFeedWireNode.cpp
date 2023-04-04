fe977ac64db074d6c59f860b6de63d33adee144b
jsb19227 <jsb19227@gmail.com>
2023-04-04 18:24:09 -0400

    asdfdsa

--- ros_stuff/baseStation/networking_stuff/src/cameraFeedWireNode.cpp
+++ ros_stuff/baseStation/networking_stuff/src/cameraFeedWireNode.cpp
@@ -5,6 +5,5 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "cameraFeedNetworkNode");
     ReceivePublisher newPublisher(WIFI_IMAGE_PORT, IMAGE_BUFFER_SIZE, "cameraFeedNetworkNodePublisher", IMAGE_DEBUG);
-    newPublisher.start();
     return 0;
 }
