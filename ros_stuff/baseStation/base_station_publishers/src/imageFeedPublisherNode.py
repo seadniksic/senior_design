a235e2ff91dca111c4b44544cdbe422fdd11b7c5
jsb19227 <jsb19227@gmail.com>
2023-04-04 17:52:42 -0400

    asdfds

--- ros_stuff/baseStation/base_station_publishers/src/imageFeedPublisherNode.py
+++ ros_stuff/baseStation/base_station_publishers/src/imageFeedPublisherNode.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy, cv2, socket, select
+from sensor_msgs.msg import String
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 import numpy as np
@@ -8,36 +9,18 @@ import numpy as np
 class ImagePublisher:
     def __init__(self):
         rospy.init_node('imagePublisher')
+        rospy.init_node('imageSubsriber')
         self.publisher_ = rospy.Publisher('imageFeedStream', Image, queue_size=10)
-        timer_period = rospy.Duration.from_sec(0.016666)  # seconds
-        self.timer = rospy.Timer(timer_period, self.timer_callback)
+        self.subscriber_ = rospy.Subscriber('cameraFeedNetworkNodePublisher', String, self.callback)
 
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind(("127.0.0.1", 8089))
-        self.socket.listen(1)
-
-        self.client = None
         self.bridge = CvBridge()
+        rospy.spin()
     
-    def timer_callback(self, event):
-        try:
-            if self.client is None:
-                readSocket, writeSocket, errorSocket = select.select([self.socket], [], [], 0)
-                if len(readSocket) == 0:
-                    return
-                self.client, addr = self.socket.accept()
-            readSocket, writeSocket, errorSocket = select.select([self.client], [], [], 0)
-            if len(readSocket) == 0:
-                return
-            size = self.client.recv(8)
-            size = int.from_bytes(size, 'little')
-            if size <= 0:
-                return
-            msg = self.client.recv(size)
-            while len(msg) < size:
-                msg += self.client.recv(size - len(msg))
-            
-            buffer = np.frombuffer(msg, np.uint8)
+    def timer_callback(self, msg):
+        try: 
+            data = bytearray()
+            data.extend(map(ord, msg))
+            buffer = np.frombuffer(data, np.uint8)
             image = cv2.imdecode(buffer, cv2.IMREAD_COLOR)
 
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -46,25 +29,13 @@ class ImagePublisher:
 
         except Exception as e:
             print("Image Publisher ran into " + str(e))
-            print("Attempting to reconnect...")
-            
-            if self.client is not None:
-                self.client.close()
-            self.client = None
-    
-    def shutdownNode(self):
-        if self.socket is not None:
-            self.socket.close()
-        if self.client is not None:
-            self.client.close()
 
 if __name__ == '__main__':
     try:
         imageNode = ImagePublisher()
 
-        rospy.spin()
     except KeyboardInterrupt:
         pass
 
     finally:
-        imageNode.shutdownNode()
+        print("Shutting down node")
