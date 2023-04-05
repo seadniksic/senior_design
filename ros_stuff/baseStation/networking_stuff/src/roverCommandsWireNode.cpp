e2734a4ff090a82cbc81bad21c94b7a73b74bb82
jsb19227 <jsb19227@gmail.com>
2023-04-04 20:11:20 -0400

    gdf

--- ros_stuff/baseStation/networking_stuff/src/roverCommandsWireNode.cpp
+++ ros_stuff/baseStation/networking_stuff/src/roverCommandsWireNode.cpp
@@ -4,6 +4,6 @@
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "mapDataNetworkNode");
-    TransmitSubscriber newNode(WIFI_ROVER_COMMANDS_PORT, HOST_IP, ROVER_COMMANDS_BUFFER_SIZE, "mapDataNetworkNodeSubscriber", COMMANDS_DEBUG);
+    TransmitSubscriber newNode(WIFI_ROVER_COMMANDS_PORT, HOST_IP, ROVER_COMMANDS_BUFFER_SIZE, "mapDataNetworkNodeTransmitter", COMMANDS_DEBUG, "mapDataNetworkNodeSubscriber");
     return 0;
 }
