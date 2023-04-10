eebdd66693d979310c27da07fbcd42d6b13c6525
ericsherman4 <esherman2018@gmail.com>
2023-04-10 13:36:54 -0400

    ros stuff

--- ros_stuff/rover/networking_stuff/src/roverCommandsWireNode.cpp
+++
@@ -1,9 +0,0 @@
-#include <ros/ros.h>
-#include "receivePublisher.h"
-
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "roverCommandsNetworkNode");
-    ReceivePublisher newPublisher(WIFI_ROVER_COMMANDS_PORT, ROVER_COMMANDS_BUFFER_SIZE, "roverCommandsNetworkNodePublisher", COMMANDS_DEBUG);
-    return 0;
-}
