0d7be73b2f4c33f2f0eff3f478411a70645b1c7e
ericsherman4 <esherman2018@gmail.com>
2023-04-14 23:27:04 -0400

    stuff

--- software/slam/localization.py
+++ software/slam/localization.py
@@ -1,12 +1,18 @@
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose
+from sensor_msgs.msg import Imu
 from std_msgs.msg import UInt8MultiArray
 import pickle
+from tf.transformations import *
 
 pubby = rospy.Publisher("cameraPositionNetworkNodeSubsriber", UInt8MultiArray, queue_size=10)
 
+offset = None
+p = None
+
 def odom_cb(msg):
+    global p
     p = msg.pose.pose
     sendData = pickle.dumps(p)
     print(len(sendData))
@@ -15,8 +21,13 @@ def odom_cb(msg):
     pubby.publish(newMsg)
     pose_pub.publish(p)
 
+def imu_cb(msg):
+    global p
+    if offset == None:
+        offset 
 
 rospy.init_node("poseSub")
 odom_sub = rospy.Subscriber('/rtabmap/odom', Odometry, odom_cb, queue_size=100)
+imu_sub = rospy.Subscriber('/rtabmap/odom', Imu, imu_cb, queue_size=100)
 pose_pub = rospy.Publisher('/rover/fused_pose', Pose, queue_size=100)
 rospy.spin()
